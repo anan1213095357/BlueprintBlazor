8db30b764a2c2d5c81e41ffeff16b55cba047ba2
奶茶叔叔 <43770875+anan1213095357@users.noreply.github.com>
2023-12-11 14:37:24 +0800

    第一次提交

---
+++ Blueprint.Demo/Models/Node.cs
@@ -0,0 +1,24 @@
+﻿namespace Blueprint.Demo.Models
+{
+    public class Node
+    {
+        public string Name { get; set; } = "";
+
+        /// <summary>
+        /// 本节点唯一标识
+        /// </summary>
+        public Guid Guid { get; set; }
+
+        /// <summary>
+        /// 输入插座
+        /// </summary>
+        public Dictionary<string, Guid> SocketIn { get; set; } = [];
+
+        /// <summary>
+        /// 输出插座
+        /// </summary>
+        public Dictionary<string, Guid> SocketOut { get; set; } = [];
+
+
+    }
+}
